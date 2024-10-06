ca03168dbb47f4779ca68713bc7827292827c679
Shariful Islam <sajukhan12905@gmail.com>
2024-10-06 21:58:20 +0600

    add worker Section

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
-const http = require("http");
-const handler = require("./helper/handleReqRes");
-const lib = require("./lib/data");
-const { sendTwilioSms } = require("./helper/notifications");
+const server = require("./lib/server");
+const worker = require("./lib/worker");
 
 const app = {};
-app.config = {
-  PORT: 5000,
-};
-app.createServer = () => {
-  const server = http.createServer(app.handleReqRes);
-  server.listen(app.config.PORT, () => {
-    console.log(`Listening to Port ${app.config.PORT}`);
-  });
+
+app.init = () => {
+  server.createServer();
+  worker.init();
 };
 
-app.handleReqRes = handler.handleReqRes;
-
-app.createServer();
-
-module.exports = app;
+app.init();
